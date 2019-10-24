3342701fbc7fc370b92dfdcc8b8a0ace9ab92198
Reserious <53444256+Reserious@users.noreply.github.com>
2019-10-24 17:34:18 +0200

    new

--- js/app.js
+++ js/app.js
@@ -145,5 +145,48 @@ $(function() {
         nav.toggleClass("show");
     });
     
+     /*Scroll*/
+    
+    var header = $("#header"),
+	 introH = $("#intro").innerHeight(),
+	 scrollOffset = $(window).scrollTop();
+	 
+	 checkScroll(scrollOffset);
+	 
+	 
+	/*Header fixed*/
+	$(window).on("scroll",function(){
+		scrollOffset = $(this).scrollTop();
+		
+		checkScroll(scrollOffset);
+		
+	});
+	
+	function checkScroll(scrollOffset){
+		
+		if(scrollOffset >= introH) {
+		 header.addClass("fixed");
+		}	else {
+                header.removeClass("fixed");
+	  }
+	}	
+	
+	/*Smooth Scroll*/
+	
+	$("[data-scroll]").on("click",function(event) {
+		event.preventDefault();
+		
+		var $this = $(this),
+		blockId = $this.data('scroll'),
+		     blockOffset = $(blockId).offset().top;
+			 
+		$("#nav a").removeClass("active");
+		$this.addClass("active");
+		
+		$("html,body").animate({
+			scrollTop:blockOffset
+		},600);
+	});
+    
 		   
 });
